a1e952fbefd9c883c54fe81d1c5a6054eb202433
lucas ramos <lucasramos1418@gmail.com>
2024-10-14 08:45:39 -0300

    feat: implementando novas funcionalidades

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Sistema de cadastro em Python
+
+<!-- ![This is an alt text.](/image/ "This is a sample image.") -->
+
+### Banco de Dados em Python:
+* Criando um banco de dados SQLite para armazenar informações do inventário doméstico.
+* implementando uma estrutura no banco de dados com uma tabela simples para gerenciar itens.
+
+### Operações CRUD (Create, Read, Update, Delete):
+* Implementando funções para inserir, visualizar, atualizar e excluir itens do inventário.
+* Integrando operações do banco de dados de maneira eficiente usando SQLite em Python.
--- main.py
+++ main.py
@@ -1,12 +1,19 @@
+# importes ###################################
+#Importando TKINTER
 from tkinter import *
 from tkinter import Tk, StringVar, ttk
 import tkinter.font as tkFont
 from tkinter import messagebox
 from tkinter import filedialog as fd
 
+#importando PILLOW
 from PIL import Image, ImageTk
 
+#Importando TKCALENDAR
+from tkcalendar import Calendar, DateEntry
+from datetime import date
 
+################################################
 # Cores ######################
 tema = "classic"
 co0 = "#2e2d2d"  # Preta
@@ -53,7 +60,8 @@ frameMeio.grid(row=1, column=0, pady=1, padx=0, sticky=NSEW)
 frameBaixo = Frame(janela, width=1043, height=300, bg=co1, relief=FLAT)
 frameBaixo.grid(row=2, column=0, pady=0, padx=1, sticky=NSEW)
 
-# abrindo imagem------------------------------------------
+# Trabalhando no frames cima ##################################################
+# abrindo imagem-----------------------------
 app_img = Image.open(r"images/inventario.png")
 app_img = app_img.resize((45, 45))
 app_img = ImageTk.PhotoImage(app_img)
@@ -72,5 +80,42 @@ app_logo = Label(
 )
 app_logo.place(x=0, y=0)
 
+# Trabalhando no frames meio ##################################################
+# criando entradas
+l_nome = Label(frameMeio, text="Nome", height=1, anchor=NW, font=("Ivy 10 bold"), bg=co1, fg=co4)
+l_nome.place(x=10, y=10)
+e_nome = Entry(frameMeio, width=30, justify="left", relief=SOLID)
+e_nome.place(x=130, y=11)
+
+l_local = Label(frameMeio, text="Sala/Área", height=1, anchor=NW, font=("Ivy 10 bold"), bg=co1, fg=co4)
+l_local.place(x=10, y=40)
+e_local = Entry(frameMeio, width=30, justify="left", relief=SOLID)
+e_local.place(x=130, y=41)
+
+l_descricao = Label(frameMeio, text="Descrição", height=1, anchor=NW, font=("Ivy 10 bold"), bg=co1, fg=co4)
+l_descricao.place(x=10, y=70)
+e_descricao = Entry(frameMeio, width=30, justify="left", relief=SOLID)
+e_descricao.place(x=130, y=71)
+
+l_model = Label(frameMeio, text="Marca/Model", height=1, anchor=NW, font=("Ivy 10 bold"), bg=co1, fg=co4)
+l_model.place(x=10, y=100)
+e_model = Entry(frameMeio, width=30, justify="left", relief=SOLID)
+e_model.place(x=130, y=101)
+
+# Frames Calendario
+l_cal = Label(frameMeio, text="Data da Compra", height=1, anchor=NW, font=("Ivy 10 bold"), bg=co1, fg=co4)
+l_cal.place(x=10, y=130)
+e_cal = DateEntry(frameMeio, width=12, background="darkblue", bordewidth=2, year=2024)
+e_cal.place(x=130, y=131)
+
+l_valor = Label(frameMeio, text="Valor da Compra", height=1, anchor=NW, font=("Ivy 10 bold"), bg=co1, fg=co4)
+l_valor.place(x=10, y=160)
+e_valor = Entry(frameMeio, width=30, justify="left", relief=SOLID)
+e_valor.place(x=130, y=161)
+
+l_serial = Label(frameMeio, text="Número de Série", height=1, anchor=NW, font=("Ivy 10 bold"), bg=co1, fg=co4)
+l_serial.place(x=10, y=190)
+e_serial = Entry(frameMeio, width=30, justify="left", relief=SOLID)
+e_serial.place(x=130, y=191)
 
 janela.mainloop()
